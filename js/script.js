fed4cecaf9e97112e10054be3d239b4e59e583ae
Uros Tomic <urostomic94@gmail.com>
2021-04-11 20:07:48 +0200

    third commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+let hamburgerMenu = document.getElementById('hamburger-menu');
+let closeIcon = document.getElementById('close-icon');
+let mobileMenu = document.querySelector('.mobile-menu');
+let mobileDropdownMenus = document.querySelectorAll('.mobile-dropdown-menu');
+function closeMobileMenu() {
+    mobileMenu.style.display = "none";
+    closeIcon.removeEventListener('click', closeMobileMenu);
+    closeIcon.style.display = "none";
+    hamburgerMenu.style.display = "block";
+    hamburgerMenu.addEventListener('click', openMobileMenu);
+
+}
+function openMobileMenu() {
+    hamburgerMenu.removeEventListener('click', openMobileMenu);
+    hamburgerMenu.style.display = "none";
+    closeIcon.style.display = "block";
+    closeIcon.addEventListener('click', closeMobileMenu);
+    mobileMenu.style.display = "flex";
+    mobileDropdownMenus.forEach(mobileDropdownMenu => {
+        mobileDropdownMenu.addEventListener("click", () => {
+            let mobileDropdownList = mobileDropdownMenu.querySelector('.mobile-dropdown-list')
+            if (mobileDropdownMenu.classList.contains('active')) {
+                mobileDropdownMenu.classList.remove('active');
+                mobileDropdownList.style.maxHeight = "0";
+            }
+            else {
+                mobileDropdownMenu.classList.add('active');
+                mobileDropdownList.style.maxHeight = "50rem";
+            }
+        })
+    });
+}
+hamburgerMenu.addEventListener('click', openMobileMenu);

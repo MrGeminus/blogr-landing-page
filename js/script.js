b0537a8e2d199ec5536f0663d57520b03156d7c0
Uros Tomic <urostomic94@gmail.com>
2021-04-12 10:33:07 +0200

    fifth commit

--- js/script.js
+++ js/script.js
@@ -3,12 +3,22 @@ let closeIcon = document.getElementById('close-icon');
 let mobileMenu = document.querySelector('.mobile-menu');
 let mobileDropdownMenus = document.querySelectorAll('.mobile-dropdown-menu');
 function closeMobileMenu() {
+    for (let i = 0; i < mobileDropdownMenus.length; i++) {
+        let mobileDropdownList = mobileDropdownMenus[i].querySelector('.mobile-dropdown-list');
+        let mobileArrow = mobileDropdownMenus[i].querySelector('.mobile-arrow');
+        let mobileDropdownTitle = mobileDropdownMenus[i].querySelector('.mobile-dropdown-title');
+        if (mobileDropdownMenus[i].classList.contains('active')) {
+            mobileDropdownMenus[i].classList.remove('active');
+            mobileDropdownList.style.maxHeight = "0";
+            mobileArrow.classList.remove('active');
+            mobileDropdownTitle.classList.remove('active');
+        }
+    }
     mobileMenu.style.display = "none";
     closeIcon.removeEventListener('click', closeMobileMenu);
     closeIcon.style.display = "none";
     hamburgerMenu.style.display = "block";
     hamburgerMenu.addEventListener('click', openMobileMenu);
-
 }
 function openMobileMenu() {
     hamburgerMenu.removeEventListener('click', openMobileMenu);
@@ -17,21 +27,41 @@ function openMobileMenu() {
     closeIcon.addEventListener('click', closeMobileMenu);
     mobileMenu.style.display = "flex";
     mobileDropdownMenus.forEach(mobileDropdownMenu => {
-        mobileDropdownMenu.addEventListener("click", () => {
+        function openDropdownMenu() {
             let mobileDropdownList = mobileDropdownMenu.querySelector('.mobile-dropdown-list');
             let mobileArrow = mobileDropdownMenu.querySelector('.mobile-arrow');
-
+            let mobileDropdownTitle = mobileDropdownMenu.querySelector('.mobile-dropdown-title');
             if (mobileDropdownMenu.classList.contains('active')) {
                 mobileDropdownMenu.classList.remove('active');
                 mobileDropdownList.style.maxHeight = "0";
                 mobileArrow.classList.remove('active');
+                mobileDropdownTitle.classList.remove('active');
             }
             else {
+                for (let i = 0; i < mobileDropdownMenus.length; i++) {
+                    let mobileDropdownList = mobileDropdownMenus[i].querySelector('.mobile-dropdown-list');
+                    let mobileArrow = mobileDropdownMenus[i].querySelector('.mobile-arrow');
+                    let mobileDropdownTitle = mobileDropdownMenus[i].querySelector('.mobile-dropdown-title');
+                    if (mobileDropdownMenus[i].classList.contains('active')) {
+                        mobileDropdownMenus[i].classList.remove('active');
+                        mobileDropdownList.style.maxHeight = "0";
+                        mobileArrow.classList.remove('active');
+                        mobileDropdownTitle.classList.remove('active');
+                    }
+                }
                 mobileDropdownMenu.classList.add('active');
                 mobileDropdownList.style.maxHeight = "50rem";
-                mobileArrow.classList.add('active')
+                mobileArrow.classList.add('active');
+                mobileDropdownTitle.classList.add('active');
             }
-        })
+        }
+        function removemobileDropdownMenuEventListener() {
+            mobileDropdownMenu.removeEventListener("click", openDropdownMenu);
+            closeIcon.removeEventListener('click', removemobileDropdownMenuEventListener);
+            closeMobileMenu();
+        }
+        mobileDropdownMenu.addEventListener("click", openDropdownMenu);
+        closeIcon.addEventListener('click', removemobileDropdownMenuEventListener);
     });
 }
 hamburgerMenu.addEventListener('click', openMobileMenu);

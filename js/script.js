030956a55eb2f1efed6c3541e8981c9ce1005d52
Uros Tomic <urostomic94@gmail.com>
2021-07-16 22:53:41 +0200

    fourteenth commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let hamburgerMenu = document.getElementById("hamburger-menu");
+document.onload = function () {
+    hamburgerMenu.addEventListener("click", openMobileMenu)
+}
+function openMobileMenu() {
+
+}

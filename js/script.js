c2b93168e9089462ec3d8f49902848e36e33b4e8
Uros Tomic <urostomic94@gmail.com>
2021-07-20 23:58:16 +0200

    sixteenth commit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 let hamburgerMenu = document.getElementById("hamburger-menu");
-document.onload = function () {
-    hamburgerMenu.addEventListener("click", openMobileMenu)
-}
-function openMobileMenu() {
+let headerMenu = document.querySelector(".header-menu");
+let dropdownTitles = document.querySelectorAll(".header__dropdown-title");
+hamburgerMenu.addEventListener("click", toggleMobileMenu)
 
+function toggleMobileMenu() {
+    hamburgerMenu.classList.toggle("hamburger-menu--open");
+    hamburgerMenu.classList.toggle("hamburger-menu--close");
+    headerMenu.classList.toggle("header-menu--active");
+    openDropDown()
+}
+
+function openDropDown() {
+    dropdownTitles.forEach(dropdownTitle => {
+        dropdownTitle.addEventListener("click", () => {
+            let dropdownTitleArrow = dropdownTitle.firstElementChild;
+            let dropdownMenuContainer = dropdownTitle.parentElement.nextElementSibling;
+            let dropdownMenu = dropdownMenuContainer.firstElementChild;
+            let dropdownLinks = dropdownMenu.querySelectorAll(".header__dropdown-list-link");
+            dropdownTitleArrow.classList.toggle("header__dropdown-arrow--active");
+            dropdownMenuContainer.classList.toggle("header__dropdown-item--active");
+            dropdownMenu.classList.toggle("header__dropdown-list--active");
+            dropdownLinks.forEach(dropdownLink => {
+                dropdownLink.classList.toggle("header__dropdown-list-link--active");
+            });
+        });
+    });
 }
